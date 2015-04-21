6f4aabc8e63929427b79e06951a4c0566dc9d730
rahulshinde <rshinde@mica.edu>
2015-04-21 02:15:00 -0400

    adding actual files

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,13 @@
 (function ($) {
 
   var getHanatarash,
-      getRandom;
+      getRandomVideo,
+      getRandomImage;
+
+  var searchString = $('#script').text();
+  var searchArray = searchString.split(" ");
+  var searchNum = Math.floor(Math.random()* searchArray.length);
+  var searchTerm = searchArray[searchNum];
 
   getHanatarash = function getHanatarashF() {
     var hanatarash = $.ajax({
@@ -16,19 +22,14 @@
           var item = data.items[i];
           var videoId = item.id.videoId;
 
-          console.log(videoId);
+          // console.log(videoId);
         }
 
     });
 
   };
 
-  getRandom = function getRandomF() {
-    var searchString = $('#script').text();
-    var searchArray = searchString.split(" ");
-    var searchNum = Math.floor(Math.random()* searchArray.length);
-    var searchTerm = searchArray[searchNum];
-
+  getRandomVideo = function getRandomF() {
     var randomVideo = $.ajax({
       url: "https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&q=" + searchTerm + "&maxResults=1&key=AIzaSyDmdYBSUbMtBe-YArhGfDR5d32RtKWwRwA",
       dataType: 'jsonp',
@@ -40,16 +41,41 @@
           var item = data.items[i];
           var videoId2 = item.id.videoId;
 
-          console.log(videoId2);
+          // console.log(videoId2);
         }   
     });
+
+
 
 
     console.log(searchNum);
     console.log(searchTerm);
   }
 
-  getRandom();
+  getImgurAuth = function getImgurAuthF() {
+    var randomImage = $.ajax({
+      url: 'https://api.imgur.com/3/gallery/search.json',
+      data: {
+        q: 'hello'
+      },
+      type: 'GET',
+      headers: {
+        Authorization: 'Client-ID 77fc4712c0786c7'
+      }
+    });
+
+    randomImage.done(function (response) {
+      var item = response.data[0];
+      var imageId = item.link;
+
+      console.log(imageId);
+      
+    });
+  }
+
+  // getHanatarash();
+  // getRandomVideo();
+  getImgurAuth();
 
 
 })(jQuery);
