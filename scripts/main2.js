3c293e2e83f51b7ebf9866420768e2acdf3daf00
rahulshinde <rshinde@mica.edu>
2015-05-01 15:44:02 -0400

    adding in videos through javascript

--- scripts/main2.js
+++ scripts/main2.js
@@ -3,11 +3,6 @@ var SITE = SITE || {};
 
 (function ($, SITE) {
 
-  var getHanatarash,
-      getRandomVideo,
-      getRandomImage;
-
-  // var searchString = $('#script').text();
   var Str1 = "Viewer as they exist in one dimensional space, Object existing in the second dimension, Time functioning as the container (imposed, never implied).";
   var Str2 = "Beginning from the entrance:";
   var Str3 = "An intervention is called for a man that we can assume to be sick. preparations are made to ensure he will feel safe. flowers placed in a porcelain vase. large sofas that will force him to sit back in his chair slouching, becoming comfortable, contributing to the degradation of his spine due to his family’s history of osteoporosis. pleasant wall decor. a photo of the dog he grew up with. subtly placed in the far corner of the room so that he will not notice it until halfway through his father’s lecture, approximately two minutes and forty three seconds before the majority of the room begins to sob.";
@@ -20,60 +15,60 @@ var SITE = SITE || {};
   var Str10 = "her attention shifts to the glass of water that the man has been holding, untouched since the conversation   began. instead, he keeps the glass still, tilted slightly to the right, his fingers placed around the rim of  the glass, so that only the water is parallel to the ground. minor vibrations slightly displace the fluid that could have been misconstrued into a metaphor about stability. a view of the ocean could be similarly repackaged into a convincing argument. movement between the glass and the seat of the sofas is split between two paths, depth shifting to exist above or beneath an established ground plane. reflection as it relates to the self. symmetry as it relates to causality (to be understood as existing in a vacuum).";
   var searchString = Str1.concat(Str2, Str3, Str4, Str5, Str6, Str7, Str8, Str9, Str10);
 
-  document.getElementById("script").textContent= Str1 + "\n\n" + Str2 + "\n\n" + Str3 + "\n\n" + Str4 + "\n\n" + Str5 + "\n\n" + Str6 + "\n\n" + Str7 + "\n\n" + Str8 + "\n\n" + Str9 + "\n\n" + Str10;
+  document.getElementById("script").innerHTML= Str1 + "<br><br>" + Str2 + "<br><br>" + Str3 + "<br><br>" + Str4 + "<br><br>" + Str5 + "<br><br>" + Str6 + "<br><br>" + Str7 + "<br><br>" + Str8 + "<br><br>" + Str9 + "<br><br>" + Str10;
 
   var searchArray = searchString.split(" ");
   var searchNum = Math.floor(Math.random()* searchArray.length);
   var searchTerm = searchArray[searchNum];
 
-  document.getElementById("variable-input").textContent= " " + searchTerm;
-  document.getElementById("variable-num").textContent= "[" + searchNum + "]";
+  infoLoaded = function infoLoadedF () {
+    // insert hanatarash
+    var videoHanatarash = '<iframe id="ytplayer" type="text/html" width="150" height="115" src="http://www.youtube.com/embed/' + SITE.hanatarash + '?autoplay=1&rel=0&showinfo=0&controls=0&origin=http://example.com" frameborder="0"/></iframe>';
+    document.getElementById('hanatarash').innerHTML=videoHanatarash;
 
-  getHanatarash = function getHanatarashF() {
-    var hanatarash = $.ajax({
+    //insert video
+    var videoRandom = '<iframe id="ytplayer" type="text/html" width="150" height="115" src="http://www.youtube.com/embed/' + SITE.hanatarash + '?autoplay=1&origin=http://example.com" frameborder="0"/></iframe>';
+
+  }
+
+  getInfo = function getInfoF () {
+    var getHanatarash,
+        getRandomVideo,
+        getRandomImage;
+
+    var getHanatarash = $.ajax({
       url: "https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&q=hanatarash+full+album&maxResults=1&key=AIzaSyDmdYBSUbMtBe-YArhGfDR5d32RtKWwRwA",
       dataType: 'jsonp',
       type: 'GET'
     });
 
-    hanatarash.done(function (data) {
+    getHanatarash.done(function (data) {
        for(var i in data.items) {
           var item = data.items[i];
-          var videoId = item.id.videoId;
-
-          document.getElementById("hanatarash-response").textContent=videoId;
-          SITE.hanatarash = videoId;
+          var hanatarashId = item.id.videoId;
+          SITE.hanatarash = hanatarashId;
+          console.log(SITE.hanatarash);
         }
 
     });
 
-  };
-
-  getRandomVideo = function getRandomF() {
-    var randomVideo = $.ajax({
+    var getRandomVideo = $.ajax({
       url: "https://www.googleapis.com/youtube/v3/search?part=id%2C+snippet&q=" + searchTerm + "&maxResults=1&key=AIzaSyDmdYBSUbMtBe-YArhGfDR5d32RtKWwRwA",
       dataType: 'jsonp',
       type: 'GET'
     });
 
-    randomVideo.done(function (data) {
+    getRandomVideo.done(function (data) {
        for(var i in data.items) {
           var item = data.items[i];
-          var videoId2 = item.id.videoId;
+          var youtubeId = item.id.videoId;
 
-          console.log(videoId2);
-
-          document.getElementById("youtube-response").textContent=videoId2;
+          SITE.video = youtubeId;
+          console.log(SITE.video);
         }   
     });
 
-
-
-
-  }
-
-  getImgurAuth = function getImgurAuthF() {
-    var randomImage = $.ajax({
+    var getRandomImage = $.ajax({
       url: 'https://api.imgur.com/3/gallery/search.json',
       data: {
         q: searchTerm
@@ -84,17 +79,26 @@ var SITE = SITE || {};
       }
     });
 
-    randomImage.done(function (response) {
+    getRandomImage.done(function (response) {
       var item = response.data[0];
       var imageId = item.link;
 
-      document.getElementById("imgur-response").textContent= imageId;
+      SITE.image = imageId;
+      console.log(SITE.image);
       
     });
+
+    $.when( getHanatarash, getRandomVideo, getRandomImage ).then(infoLoaded);
+
   }
 
-  getHanatarash();
-  getRandomVideo();
-  getImgurAuth(); 
+
+  document.getElementById("variable-input").textContent= " " + searchTerm;
+  document.getElementById("variable-num").textContent= "[" + searchNum + "]";
+
+
+
+  getInfo();
+  infoLoaded();
 
 })(jQuery, SITE);
