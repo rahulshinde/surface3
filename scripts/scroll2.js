34650d32210f4ae43fd0ce54325712958a29b422
rahulshinde <rshinde@mica.edu>
2015-05-13 00:32:22 -0600

    randomizing 3d translations

--- scripts/scroll2.js
+++ scripts/scroll2.js
@@ -1,5 +1,28 @@
 var lastScrollTop = 0;
+
+var marqueeXFactor = (Math.random()*1-1).toFixed(1);
+var marqueeYFactor = (Math.random()*1-1).toFixed(1);
+var marqueeZFactor = (Math.random()*1-1).toFixed(1);
+
+var videoXFactor = (Math.random()*1-1).toFixed(1);
+var videoYFactor = (Math.random()*1-1).toFixed(1);
+var videoZFactor = (Math.random()*1-1).toFixed(1);
+
+var imgXFactor = (Math.random()*1-1).toFixed(1);
+var imgYFactor = (Math.random()*1-1).toFixed(1);
+var imgZFactor = (Math.random()*1-1).toFixed(1);
+
+var marqueeDeg = (Math.random()*100-100);
+var marqueeXPosition = (Math.random()*180-180);
+
+var videoDeg = (Math.random()*100-100);
+var videoXPosition = (Math.random()*180-180);
+
+var imgDeg = (Math.random()*100-100);
+var imgXPosition = (Math.random()*180-180);
+
 scrolled = 0, // amount window has scrolled
+
 
 $(function(){
     transformProp = Modernizr.prefixed('transform'); // ie: WebkitTransform
@@ -21,42 +44,42 @@ $(function(){
 });
 
 function transformMarquee( scroll ) {
-  var deg = scroll * -130;
-  var rotateMarquee = 'rotate3d(-0.3, 0.9, -0.5,' + deg + "deg)";
+  var deg = scroll * marqueeDeg;
+  var rotateMarquee = 'rotate3d(' + marqueeXFactor + ',' + marqueeYFactor + ',' + marqueeZFactor + ',' + deg +'deg)';
 
-  var translateX = scroll * -5;
-  var translateY = scroll * -10;
-  var translateZ = scroll * 20;
+  var translateX = scroll * marqueeXPosition;
+  var translateY = scroll * -70;
+  var translateZ = scroll * 230;
   var translateMarquee = 'translate3d(' + translateX + 'px, ' + translateY + 'px, ' + translateZ + 'px)';
   var transformMarquee = rotateMarquee + ' ' + translateMarquee;
-  console.log(transformMarquee);
   $('#marqueecontainer').css({'-moz-transform' : transformMarquee, '-webkit-transform' : transformMarquee});
+  console.log(transformMarquee);
   // $('#marqueecontainer').css({'-moz-transform' : translateMarquee, '-webkit-transform' : translateMarquee});
 };
 
 function transformVideo( scroll ) {
-  var deg = scroll * -50;
-  var rotateMarquee = 'rotate3d(-1, .2, .5,' + deg + "deg)";
+  var deg = scroll * videoDeg;
+  var rotateMarquee = 'rotate3d(' + videoXFactor + ',' + videoYFactor + ',' + videoZFactor + ',' + deg +'deg)';
 
-  var translateX = scroll * -5;
-  var translateY = scroll * -10;
-  var translateZ = scroll * 20;
+  var translateX = scroll * videoXPosition;
+  var translateY = scroll * -30;
+  var translateZ = scroll * -50;
   var translateMarquee = 'translate3d(' + translateX + 'px, ' + translateY + 'px, ' + translateZ + 'px)';
-
-  $('#video').css({'-moz-transform' : rotateMarquee, '-webkit-transform' : rotateMarquee});
+  var transformMarquee = rotateMarquee + ' ' + translateMarquee;
+  $('#video').css({'-moz-transform' : transformMarquee, '-webkit-transform' : transformMarquee});
   // $('#marqueecontainer').css({'-moz-transform' : translateMarquee, '-webkit-transform' : translateMarquee});
 };
 
 function transformImg( scroll ) {
-  var deg = scroll * 100;
-  var rotateMarquee = 'rotate3d(-.2, .2, .2,' + deg + "deg)";
+  var deg = scroll * imgDeg;
+  var rotateMarquee = 'rotate3d(' + imgXFactor + ',' + imgYFactor + ',' + imgZFactor + ',' + deg +'deg)';
 
-  var translateX = scroll * -5;
-  var translateY = scroll * -10;
+  var translateX = scroll * imgXPosition;
+  var translateY = scroll * 20;
   var translateZ = scroll * 20;
   var translateMarquee = 'translate3d(' + translateX + 'px, ' + translateY + 'px, ' + translateZ + 'px)';
-
-  $('#img').css({'-moz-transform' : rotateMarquee, '-webkit-transform' : rotateMarquee});
+  var transformMarquee = rotateMarquee + ' ' + translateMarquee;
+  $('#img').css({'-moz-transform' : transformMarquee, '-webkit-transform' : transformMarquee});
   // $('#marqueecontainer').css({'-moz-transform' : translateMarquee, '-webkit-transform' : translateMarquee});
 };
 
